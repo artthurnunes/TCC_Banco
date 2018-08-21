34c033fddd44ac35b29fc61eb3a7636e15f85d8d
artthur.nunes <artthur.nunes@outlook.com>
2018-08-21 13:31:48 -0300

    Adicinando Inserts

--- scriptsInserts.sql
+++ scriptsInserts.sql
@@ -1,15 +1,67 @@
 INSERT INTO TB_ALUNOS (SITUACAO,NOME,TEL1,TEL2,PROFISSAO,SEXO,ESTADO_CIVIL,RG,CPF,DT_NASCIMENTO 
 					   ,NM_MAE,NM_PAI,NM_EMER,TEL_EMER,PARENTESCO,END_RUA,END_NUMERO,END_BAIRRO,END_CIDADE 
 					   ,END_ESTADO,END_CEP) 
-			VALUES (TRUE,'Damiana Nunes','(19) 3887-7450','(19) 98387-7421','Assistente','Feminino',1,'13.874.741','845.547.841-31'
-				    ,'01/02/1987','Maria das Dores','João da Silva','Benedito Souza','(19) 3598-5412',1,'Rua Alvares de Carvalho','1205'
+			VALUES (TRUE,'Damiana Nunes','(19) 5413-5417','(19) 98387-7421','Farmacêutica','Feminino',1,'13.874.741','845.547.841-31'
+				    ,'25/08/1990','Maria das Dores','João da Silva','Benedito Souza','(19) 3598-5412',1,'Rua Alvares de Carvalho','1205'
                     ,'Jd St Ângelo','Sumaré',5,'13.654-541'),
-                    (TRUE,'Arthur Nunes','(19) 3887-7450','(19) 98387-7421','Assistente','Masculino',2,'13.874.741','846.741.554-54'
-				    ,'01/02/1987','Maria das Dores','João da Silva','Benedito Souza','(19) 3598-5412',3,'Rua Alvares de Carvalho','1205'
-                    ,'Jd St Ângelo','Sumaré',3,'13.654-541'),
-                    (TRUE,'Arizeu Santana','(19) 3887-7450','(19) 98387-7421','Empresário','Masculino',2,'13.874.741','451.574.841-41'
-				    ,'01/02/1987','Maria das Dores','João da Silva','Benedito Souza','(19) 3598-5412',3,'Rua Alvares de Carvalho','1205'
-                    ,'Jd St Ângelo','Sumaré',3,'13.654-541');
+                    (TRUE,'Arthur Nunes','(19) 3887-7450','(19) 98414-5413','Assistênte de Sistemas','Masculino',2,'54.515.547','864.541.574-51'
+				    ,'01/02/1987','Celia Maria Pereira','Roberto Américo Nunes','Cleisson Duarte','(19) 3541-5413',3,'Rua Olinda','58'
+                    ,'Centro','Sumaré',5,'13.654-541'),
+                    (TRUE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','451.574.841-41'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    -- modificar daqui pra baixo
+                    (FALSE,'A','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','1'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','2'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','3'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','4'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','5'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','6'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','7'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','8'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','9'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','10'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','11'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','12'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','13'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','14'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','15'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','16'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541'),
+                    (FALSE,'Arizeu Santana','(19) 5412-5416','(19) 97842-4127','Empresário','Masculino',2,'17.541.571-X','17'
+				    ,'01/12/1970','Joana de Carvalho','Rafael Nascimento','Ricardo Soares','(19) 3578-5746',4,'Av João Darco','2548'
+                    ,'Cambaré','Americana',8,'15.548-541');
                        
 INSERT INTO TB_ALUNOS (NOME,CPF) VALUES ('A','2');
 
